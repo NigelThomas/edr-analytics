9b88030278e7999d7c36f20ec1d68daa38fc37ca
NigelThomas <nigel.cl.thomas@gmail.com>
2020-02-04 14:40:02 +0000

    get TAC data from TAC database

--- test/generate_data.py
+++ test/generate_data.py
@@ -1,31 +1,48 @@
 import random
 import time
 import argparse
+import csv
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c","--subscriber_count", type=int, default=1000, help="number of subscribers to be created")
-parser.add_argument("-t","--output_time", type=int, default=10000, help="number of seconds of calls")
+parser.add_argument("-o","--output_time", type=int, default=10000, help="number of seconds of calls")
 parser.add_argument("-r","--call_rate", type=int, default=10, help="max number of calls per second")
 parser.add_argument("-u","--upgrade_prob", type=int, default=5, help="integer pct probability of upgrade")
 
 parser.add_argument( "-k", "--trickle", default=True, action='store_true', help="Trickle one second of data each second")
 parser.add_argument( "-n", "--no_trickle", default=False, dest='trickle', action='store_false', help="No trickling - emit data immediately")
 
+parser.add_argument('-t', '--tac_file', default='./test/tac.csv', help='input: TAC database for generating random TACs')
+parser.add_argument('-T', '--tac_count', default=0, help='Number of distinct TACs')
 parser.add_argument('-s', '--subscriber_file', default='./subscribers.csv', help='output: initial list of generated subscribers')
 parser.add_argument('-e', '--edr_file', default='./calldata.csv', help='output: generated file of EDRs')
 
 args = parser.parse_args()
 
 
+# TAC|Marketing Name|Manufacturer (or) Applicant|Bands|5G Bands|LPWAN|Radio Interface|Brand Name|Model Name|Operating System|NFC|Bluetooth|WLAN|Device Type|Removable UICC|Removable EUICC|NonRemovable UICC|NonRemovable EUICC|Simslot|Imeiquantitysupport
+
+tacs = []
+tac_descs = []
+tac=0
+
+with open(args.tac_file, "r") as tf:
+    tfreader = csv.DictReader(tf, delimiter='|')
+
+    for row in tfreader:
+        tac_desc = {'code':row['TAC'],'model':row['Model Name'],'band':row['Bands']}
+        tac_descs.append(tac_desc)
+        tacs.append(tac)
+
+        tac += 1
+        if args.tac_count > 0 and tac >= args.tac_count:
+            break
+
+    tf.close()
+
 # just number the subscribers
 # treat subscriber number as msisdn
 subscribers = [i for i in range(args.subscriber_count)]
-
-tacs = [i for i in range(3)]
-
-tac_descs = [{'code':'01233900','model':'iPhone 4', 'band':'3G'}
-            ,{'code':'35357505','model':'Samsung GALAXY S3 GT-I9305','band':'LTE'}
-            ,{'code':'35549706','model':'Motorola Moto G','band':'3G'}]
 
 subscriber_tacs = []
 subscriber_tacnos = []
@@ -36,7 +53,7 @@ subscriber_imeis = []
 sf = open(args.subscriber_file,"w")
 
 
-sf.write("msisdn,imei,band\n")
+sf.write("msisdn|imei|band\n")
 
 for sub in range(len(subscribers)):
     tac = random.randrange(len(tacs))
@@ -47,7 +64,7 @@ for sub in range(len(subscribers)):
     tac_desc = tac_descs[tac]
     band = tac_desc['band']
 
-    sf.write("%010d,%s,%s\n"% (sub, imei, band))
+    sf.write("%010d|%s|%s\n"% (sub, imei, band))
 
 sf.close
 
@@ -61,7 +78,7 @@ startsecs = time.time()
 
 ef = open(args.edr_file,"w")
 
-ef.write("calltime,msisdn,imei,band,upg_imei\n")
+ef.write("calltime|msisdn|imei|band|upg_imei\n")
 
 for calltime in range(args.output_time):
 
@@ -89,7 +106,7 @@ for calltime in range(args.output_time):
         tac_desc = tac_descs[tac]
         band = tac_desc['band']
 
-        ef.write("%010d,%010d,%s,%s,%d\n"% (calltime+startsecs, sub, imei, band, upgrade_imei))
+        ef.write("%010d|%010d|%s|%s|%d\n"% (calltime+startsecs, sub, imei, band, upgrade_imei))
     
    
     # If we want to trickle the data:
